06133e3e728fd225d941398c019813d867131feb
Jonah <jonahkf@gmail.com>
2024-10-28 21:22:31 +0900

    ADDED: toDoCreator.js code, index.js screen control
    toDoCreator now creates ToDo items and lists of
    ToDo items. Lists are with indexes.
    index.js controlled by screenController() function.
    Code can now read titles for Lists and Tasks, and
    display them in the DOM sidebar.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,51 @@
 import "./styles.css";
-import "./toDoCreator.js"
+import { ToDoList } from "./toDoCreator.js"
 
-// DOM Methods Below
+function screenController() {
+    const listNav = document.querySelector(".list-nav");
+    const pageBody = document.querySelector(".container");
+
+    const defaultList = new ToDoList("default");
+    const defaultTaskIndex = defaultList.addToDo("Task Title", "default");
+
+
+    const displayList = (list) => {
+        // Displays List name in sidebar
+        const navListContainer = document.createElement("div");
+        navListContainer.classList.add("nav-list-container");
+        listNav.appendChild(navListContainer);
+        const navListHeader = document.createElement("div");
+        navListHeader.classList.add("nav-list-header");
+        navListHeader.textContent = list.Name;
+        navListContainer.appendChild(navListHeader);
+
+        const taskArray = list.getAllToDoItems();
+
+        // Displays list's Tasks in sidebar
+        taskArray.forEach(ToDo => {
+            const task = document.createElement("div");
+            task.classList.add("nav-list-child");
+            task.textContent = ToDo.title;
+            navListContainer.appendChild(task);
+
+            // Attach event listener for click
+            task.addEventListener("click", clickTask);
+        });
+    }
+
+    const newList = () => {
+
+    }
+
+    const clickTask = () => {
+        // Changes contents of .container to match contents of clicked Task
+    }
+
+    // Test ToDo Item
+    displayList(defaultList);
+
+}
+
+
+
+screenController();
--- src/toDoCreator.js
+++ src/toDoCreator.js
@@ -1,35 +1,42 @@
 class ToDo {
   constructor(
     title,
+    project = "default",
     description = "",
     dueDate = "",
     priority = "",
     notes = "",
-    checklist = "",
-    project = "default"
+    checklist = ""
   ) {
     this.title = title;
+    this.project = project;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
     this.notes = notes;
     this.checklist = checklist;
-    this.project = project;
   }
 
   toJSON() {
     return {
-        // Convert to JSON here
-        
+        //Converts to JSON
+        title: this.title,
+        project: this.project,
+        description: this.description,
+        dueDate: this.dueDate,
+        priority: this.priority,
+        notes: this.notes,
+        checklist: this.checklist
     };
   }
 }
 
 // Controller Class
-class ToDoController {
-    constructor () {
-        this.ToDoList = new Map();
+class ToDoList {
+    constructor (name) {
+        this.ToDoList = new Map(); // Using Maps, recommended by StackOverflow
         this.Index = 0;
+        this.Name = name;
     }
 
     checkTitle (title) {
@@ -39,14 +46,31 @@ class ToDoController {
         }
     }
 
+    // Returns JSON of ToDo Item, searched by index
+    getToDoItem (index) {
+        const ToDoItem = this.ToDoList.get(index);
+        return ToDoItem.toJSON();
+    }
+
+    getAllToDoItems() {
+        // .values() iterates over a map, then .map() turns the JSONs into an array.
+        return Array.from(this.ToDoList.values()).map(todo => todo.toJSON());
+    }
+
     increaseIndex () {
         return this.Index++;
     }
 
     addToDo (title, project = "default") {
         this.checkTitle(title);
+        const index = this.increaseIndex();
+        const toDo = new ToDo(title, project);
+        this.ToDoList.set(index, toDo);
+
+        // Return index for use in DOM later
+        return index;
     }
 
-    
 }
 
+export { ToDo, ToDoList };
